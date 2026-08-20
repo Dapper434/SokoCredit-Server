e5b61796eff98ea8389272dfe301680db58d684f
sydomori <sydneyomori@gmail.com>
2026-08-21 00:37:21 +0300

    feat: assemble app with config setting, bound extensions & routes

---
+++ backend/app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+import os
+
+from config import config_by_name
+from extensions import db, migrate, mail, jwt, cors
+
+#use app factory pattern to create the app instance, and to allow for different configurations to be used in different environments
+def create_app(config_name : str = None) -> Flask:
+    """
+    match the config_name to the appropriate config class and initialize the app with it
+    if config_name is not provided, check the FLASK_ENV environment variable
+    or default to "development"
+    """
+    config_name = config_name or os.environ.get("FLASK_ENV", "development")
+
+    app = Flask(__name__)
+    #load all settings from the config class into the app's config
+    app.config.from_object(config_by_name[config_name])
+
+    #initialize the extensions with the app
+    db.init_app(app)
+    migrate.init_app(app, db)
+    mail.init_app(app)
+    jwt.init_app(app)
+    cors.init_app(app,origins=app.config["CORS_ORIGINS"], supports_credentials=True)
+    mail.init_app(app)
+
+    """import models here to avoid circular imports, and to ensure they are registered with SQLAlchemy"""
+    from foundation import models as foundation_models
+
+    from foundation.routes import foundation_bp
+    app.register_blueprint(foundation_bp, url_prefix="/api/auth")
+
+    from foundation.auth import register_jwt_callbacks
+    register_jwt_callbacks(jwt)
+
+    #provide a working route you can visit to check if the app is running, and to test the health of the app
+    @app.get("/api/health")
+    def health():
+        return {"status": "ok"}
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
