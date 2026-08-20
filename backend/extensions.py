2ef70c2c3ca06c84ebf97fa584dd9515a5fe7f4d
sydomori <sydneyomori@gmail.com>
2026-08-20 22:41:44 +0300

    feat: set up flask extensions

---
+++ backend/extensions.py
@@ -0,0 +1,13 @@
+""" single source of truth for all extensions used in the app """
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+
+db = SQLAlchemy()
+migrate = Migrate()
+mail = Mail()
+jwt = JWTManager()
+cors = CORS()
