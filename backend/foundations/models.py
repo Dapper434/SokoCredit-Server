6829a74c823cace1b7dd3e2db702bf44e6c9236a
sydomori <sydneyomori@gmail.com>
2026-08-21 02:37:42 +0300

    feat: create AuditLog model

--- backend/foundations/models.py
+++ backend/foundations/models.py
@@ -56,3 +56,37 @@ class User(db.Model):
  
     def __repr__(self):
         return f"<User {self.id} {self.email} org={self.organization_id}>"
+
+class AuditLog(db.Model):
+    __tablename__ = "audit_logs"
+ 
+    id = db.Column(db.Integer, primary_key=True)
+    #each audit log is associated with an organization
+    organization_id = db.Column(
+        db.Integer, db.ForeignKey("organizations.id"), nullable=False, index=True
+    )
+
+    #is nullable as an action can be system created
+    actor_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True, index=True)
+
+    """ 
+    generic pointers to any row in any table
+    allows log_action() to be reuseable across every module
+    """
+    entity_type = db.Column(db.String(100), nullable=False, index=True)
+    entity_id = db.Column(db.Integer, nullable=False, index=True)
+
+    #traces the action performed on the entity, and the state of the entity before and after the action
+    action = db.Column(db.String(20), nullable=False)
+
+    #before and after state of the entity
+    #create: before -> null , after -> new entity
+    #update: before -> old entity, after -> new entity
+    #delete: before -> old entity, after -> null
+    before = db.Column(db.JSON, nullable=True)
+    after = db.Column(db.JSON, nullable=True)
+ 
+    created_at = db.Column(db.DateTime(timezone=True), default=utcnow, nullable=False, index=True)
+ 
+    def __repr__(self):
+        return f"<AuditLog {self.entity_type}#{self.entity_id} {self.action}>"
