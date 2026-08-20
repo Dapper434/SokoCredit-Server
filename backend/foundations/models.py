330807a2a1ca8fc9df4ea3973b28f78b7c043c6f
sydomori <sydneyomori@gmail.com>
2026-08-21 02:12:30 +0300

    feat: create user model

--- backend/foundations/models.py
+++ backend/foundations/models.py
@@ -25,3 +25,34 @@ class Organisation(db.Model):
  
     def __repr__(self):
         return f"<Organization {self.id} {self.slug}>"
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    #enforce table level guard rail
+    __table_args__ = (
+        db.CheckConstraint(f"role IN {ROLES}", name="ck_users_role_valid"),
+    )
+    
+    id = db.Column(db.Integer, primary_key=True)
+
+    #makes sure you cant create a user pointing to an organisation that doesnt exist, and also makes sure you cant delete an organisation that has users 
+    organization_id = db.Column(
+        db.Integer, db.ForeignKey("organizations.id"), nullable=False, index=True
+    )
+ 
+    email = db.Column(db.String(255), unique=True, nullable=False, index=True)
+    password_hash = db.Column(db.String(255), nullable=False)
+    full_name = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(20), nullable=False, default="loan_officer")
+ 
+    is_active = db.Column(db.Boolean, nullable=False, default=True)
+    created_at = db.Column(db.DateTime(timezone=True), default=utcnow, nullable=False)
+    last_login_at = db.Column(db.DateTime(timezone=True), nullable=True)
+
+    #one to many relationship with organizations, back_populates to allow for bidirectional access
+    organization = db.relationship("Organization", back_populates="users")
+ 
+    def __repr__(self):
+        return f"<User {self.id} {self.email} org={self.organization_id}>"
